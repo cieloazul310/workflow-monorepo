9e9a99a4e067da06a35660906a76b807d45deb8f
cieloazul310 <cieloazul310@gmail.com>
2022-10-07 15:43:54 +0900

    fix(mono-two): divide code

--- packages/mono-two/src/index.ts
+++ packages/mono-two/src/index.ts
@@ -1,9 +1,1 @@
-export function add(...values: number[]) {
-  if (!values.length) return 0;
-  return values.reduce((accum, curr) => accum + curr, 0);
-}
-
-export function multiply(...values: number[]) {
-  if (!values.length) return 1;
-  return values.reduce((accum, curr) => accum * curr, 1);
-}
+export * from './calc';
